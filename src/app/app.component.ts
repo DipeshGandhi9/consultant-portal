c35d9e1b45933fd73cd5cc139b5dae4114ed0364
dipesh <dipesh_gandhi9@yahoo.com>
2023-01-18 13:54:52 +0530

    Logo and loader implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, Renderer2 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { TranslateService } from '@ngx-translate/core';
 
 import translateLang from './../assets/i18n/en.json';
@@ -14,10 +15,16 @@ export class AppComponent implements OnInit {
 
   constructor(
     public translate: TranslateService,
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2,
   ) {
     translate.addLangs(['en']);
     translate.setDefaultLang('en');
     translate.use('en');
     translate.setTranslation('en', translateLang);
+  }
+
+  ngAfterViewInit() {
+    this.renderer.setStyle(document.getElementById('preloader'), 'display', 'none');
   }
 }
